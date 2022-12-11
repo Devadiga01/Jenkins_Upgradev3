e0df189bb07312d6661ad3a0d76e9fa50fbc0e42
Ritdev <ritheshdevadiga1999@gmail.com>
2022-12-11 17:55:15 +0000

    New Line modified

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hi & Good Evening This is RITHESH DEVADIGA !!!");
+	  System.out.println("Hello from Main Method & Hi & Good Evening This is RITHESH DEVADIGA ....Thanks for joining!!!");
   }
 	
 }
